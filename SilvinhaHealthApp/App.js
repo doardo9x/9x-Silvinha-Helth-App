5c67a3a0450ecbbc21483190f3e4806fd445601f
doardo9x <eduardo010304@gmail.com>
2024-03-04 12:29:18 -0300

    Oiiii

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Title from './src/components/Title';
+import Form from './src/components/Form';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title />
+      <Form/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,13 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e0e5e5',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#191825',
+    paddingTop: 80,
   },
-  title:{
-    color: '#FF0043',
-    fontSize: 28,
-    fontWeight: "bold"
-  }
 });
